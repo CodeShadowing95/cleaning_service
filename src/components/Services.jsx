9de8fef6bc657f81cb01d003a6a0dbf57ee3067d
Patrick NAMEGNI <49642047+CodeShadowing95@users.noreply.github.com>
2024-11-14 12:39:59 +0100

    New changes on Service - 3

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -20,7 +20,7 @@ const Services = () => {
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M16.72 10.43C14.68 8.39 14.5 4.66 14.5 4H13v2H9V4H7c0-1.1.9-2 2-2h7v1c0 .08.04 4.63 1.78 6.37zM17 2v2h1V2zm-2 10c-2-2-2-5-2-5H9v2c0 1 0 1-1 2s-1 2-1 2v7c0 1.1.9 2 2 2h4c1.1 0 2-.9 2-2z"/></svg>
             </div>
             <p className="text-base font-semibold text-white mb-1">La fenêtre est claire</p>
-            <p className="text-[13px] text-white text-pretty font-light text-center mb-10">Découvrez une panoplie de nos services de nettoyage adaptés à vos besoins et à vos budgets.</p>
+            <p className="text-[13px] text-white text-pretty font-light text-center mb-10">Découvrez toutes nos panoplies de services adaptés à vos besoins et à vos budgets.</p>
             <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-xs font-medium text-white backdrop-blur-3xl">
@@ -61,7 +61,7 @@ const Services = () => {
               <img src="/services/service.jpg" alt="service" className="w-full h-full object-cover" />
               <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/80">
                 <div className="w-full h-full flex flex-col justify-end items-start p-4">
-                  <p className="text-xl text-left text-white font-bold">Aération de vos locaux</p>
+                  <p className="text-xl text-left text-white font-bold">Nettoyage de sol et entretien des murs</p>
                   <div className="w-[20%] h-1 bg-green-400 mt-2" />
                 </div>
               </div>
@@ -69,7 +69,7 @@ const Services = () => {
             {/* Card Back */}
             <div id="flip-card-back" className="absolute w-full h-full rounded-lg overflow-hidden">
               <div className="w-full h-full flex flex-col items-center gap-4 bg-white px-8 py-10">
-                <p className="text-lg font-bold max-w-[250px]">Aération de vos locaux</p>
+                <p className="text-lg font-bold max-w-[250px]">Nettoyage de sol et entretien des murs</p>
                 <p className="text-sm text-gray-500 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti iste a fuga veniam voluptatibus, rem ullam labore ea totam dignissimos explicabo officiis quaerat facilis quasi placeat dolorum sapiente, veritatis quisquam.</p>
               </div>
             </div>
@@ -146,32 +146,32 @@ const Services = () => {
         </div>
 
         {/* Card */}
-        <div id="flip-card" className="flex justify-center items-center relative w-[320px] h-[350px] rounded-lg cursor-pointer">
+        {/* <div id="flip-card" className="flex justify-center items-center relative w-[320px] h-[350px] rounded-lg cursor-pointer">
           <div id="flip-card-inner" className="relative w-full h-full text-center transition-transform duration-500 shadow-md rounded-lg">
-            {/* Card Front */}
+
             <div id="flip-card-front" className="absolute w-full h-full overflow-hidden rounded-lg">
               <img src="/services/service.jpg" alt="service" className="w-full h-full object-cover" />
               <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/80">
                 <div className="w-full h-full flex flex-col justify-end items-start p-4">
-                  <p className="text-xl text-left text-white font-bold">Nettoyage après travaux de construction</p>
+                  <p className="text-xl text-left text-white font-bold">Nettoyage de fin de chantier</p>
                   <div className="w-[20%] h-1 bg-orange-400 mt-2" />
                 </div>
               </div>
             </div>
-            {/* Card Back */}
+
             <div id="flip-card-back" className="absolute w-full h-full rounded-lg overflow-hidden">
               <div className="w-full h-full flex flex-col items-center gap-4 bg-white px-4 py-10">
-                <p className="text-lg font-bold max-w-[250px]">Nettoyage après travaux de construction</p>
+                <p className="text-lg font-bold max-w-[250px]">Nettoyage de fin de chantier</p>
                 <p className="text-sm text-gray-500 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti iste a fuga veniam voluptatibus, rem ullam labore ea totam dignissimos explicabo officiis quaerat facilis quasi placeat dolorum sapiente, veritatis quisquam.</p>
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* Card */}
-        <div id="flip-card" className="flex justify-center items-center relative w-[320px] h-[350px] rounded-lg cursor-pointer">
+        {/* <div id="flip-card" className="flex justify-center items-center relative w-[320px] h-[350px] rounded-lg cursor-pointer">
           <div id="flip-card-inner" className="relative w-full h-full text-center transition-transform duration-500 shadow-md rounded-lg">
-            {/* Card Front */}
+
             <div id="flip-card-front" className="absolute w-full h-full overflow-hidden rounded-lg">
               <img src="/services/service.jpg" alt="service" className="w-full h-full object-cover" />
               <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/80">
@@ -181,7 +181,7 @@ const Services = () => {
                 </div>
               </div>
             </div>
-            {/* Card Back */}
+
             <div id="flip-card-back" className="absolute w-full h-full rounded-lg overflow-hidden">
               <div className="w-full h-full flex flex-col items-center gap-4 bg-white px-4 py-10">
                 <p className="text-lg font-bold max-w-[250px]">Fourniture de matériels sanitaires</p>
@@ -189,8 +189,12 @@ const Services = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
+      </div>
+
+      <div className="w-full flex border p-8">
+        Cool
       </div>
     </div>
   )
--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -18,7 +18,7 @@ const WhyChooseUs = () => {
         <div className="flex flex-col gap-3">
           <img src="/service.png" alt="service" className="w-12 h-12" />
           <p className="text-sm font-semibold m-0 p-0 capitalize">Service sur mesure</p>
-          <p className="text-[13px] font-medium m-0 p-0">Nous comprenons que chaque espace et chaque client est unique. C’est pourquoi nous adaptons nos services de nettoyage à vos besoins spécifiques, pour un résultat qui vous satisfait pleinement.</p>
+          <p className="text-[13px] font-normal m-0 p-0">Nous comprenons que chaque espace et chaque client est unique. C’est pourquoi nous adaptons nos services de nettoyage à vos besoins spécifiques, pour un résultat qui vous satisfait pleinement.</p>
           <img src="/wcu1.jpg" alt="desc1" className="rounded-xl mt-6 w-full h-full object-cover" />
         </div>
         
@@ -27,14 +27,14 @@ const WhyChooseUs = () => {
           <img src="/team.jpg" alt="desc1" className="rounded-xl mb-6 w-full h-full object-cover" />
           <img src="/team-building.png" alt="service" className="w-12 h-12" />
           <p className="text-sm font-semibold m-0 p-0 capitalize">Équipe de confiance</p>
-          <p className="text-[13px] font-medium m-0 p-0">Notre équipe, formée et passionnée, est engagée à offrir un service fiable et de qualité. Vous pouvez compter sur notre professionnalisme et notre souci du détail à chaque intervention.</p>
+          <p className="text-[13px] font-normal m-0 p-0">Notre équipe, formée et passionnée, est engagée à offrir un service fiable et de qualité. Vous pouvez compter sur notre professionnalisme et notre souci du détail à chaque intervention.</p>
         </div>
         
         {/* 3rd panel */}
         <div className="flex flex-col gap-3">
           <img src="/cleaning.png" alt="service" className="w-12 h-12" />
           <p className="text-sm font-semibold m-0 p-0 capitalize">Produits écologiques et parfumés</p>
-          <p className="text-[13px] font-medium m-0 p-0">Nous utilisons des produits de nettoyage respectueux de l’environnement, combinant efficacité et parfums agréables pour un espace non seulement propre, mais aussi rafraîchi par des senteurs subtiles et durables.</p>
+          <p className="text-[13px] font-normal m-0 p-0">Nous utilisons des produits de nettoyage respectueux de l’environnement, combinant efficacité et parfums agréables pour un espace non seulement propre, mais aussi rafraîchi par des senteurs subtiles et durables.</p>
           <img src="/items.jpg" alt="desc1" className="rounded-xl mt-6 w-full h-full object-cover" />
         </div>
         
@@ -43,7 +43,7 @@ const WhyChooseUs = () => {
           <img src="/flexibilite.jpg" alt="desc1" className="rounded-xl mb-6 w-full h-full" />
           <img src="/flexible.png" alt="service" className="w-12 h-12" />
           <p className="text-sm font-semibold m-0 p-0 capitalize">Flexibilité et réactivité</p>
-          <p className="text-[13px] font-medium m-0 p-0">Nous savons nous adapter rapidement à vos besoins, vos budgets et à vos contraintes de temps. Disponibles et réactifs, nous faisons de votre satisfaction notre priorité.</p>
+          <p className="text-[13px] font-normal m-0 p-0">Nous savons nous adapter rapidement à vos besoins, vos budgets et à vos contraintes de temps. Disponibles et réactifs, nous faisons de votre satisfaction notre priorité.</p>
         </div>
 
         <img src="/sparkles.png" alt="sparkles" className="absolute bottom-[30%] -right-14 -rotate-[20deg] w-14 h-14 animate-spin " />
