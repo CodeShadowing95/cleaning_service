dde3a85081d7ea4e608b16c2a116b595ff17498b
VastoLorde <ksenbonsakura@gmail.com>
2024-11-15 04:04:04 +0100

    Update Equipments

--- src/components/Equipments.jsx
+++ src/components/Equipments.jsx
@@ -1,7 +1,9 @@
 
 const Equipments = () => {
   return (
-    <div className="w-full h-screen bg-white px-[130px] py-10"></div>
+    <div className="w-full h-screen bg-white px-[130px] py-10">
+      
+    </div>
   )
 }
 
