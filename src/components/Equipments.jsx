8cba34a1bf4734416315245f9976e3e2e6d5496c
Patrick NAMEGNI <49642047+CodeShadowing95@users.noreply.github.com>
2024-12-09 17:18:30 +0100

    UPDATE - Fixing components disposition

--- src/components/Equipments.jsx
+++ src/components/Equipments.jsx
@@ -16,7 +16,7 @@ const Equipments = () => {
   };
 
   return (
-    <div id="equipments" className="w-full my-10 bg-white py-24 relative">
+    <div id="equipments" className="w-full my-20 bg-white py-10 relative overflow-hidden">
       {/* Banner wallpaper */}
       <Marquee className="absolute w-full h-full top-0 left-0 z-0" pauseOnHover={false} speed={40} gradient={false}>
           <img src="/equipments/wallpaper3.jpg" alt="wallpaper" className="w-full h-full object-cover" />
